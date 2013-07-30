076747ceefa31e7fcfd4c296c44a55760cba5120
Mauricio Poppe <me@maurizzzio.com>
2013-07-30 18:02:52 -0400

    Distinct types of particles

--- js/ParticleFactory.js
+++ js/ParticleFactory.js
@@ -9,33 +9,38 @@ Ape.ParticleFactory = function (type) {
     var particle = new Ape.Particle();
     switch (type) {
         case 'pistol':
+            particle.color = 'green';
             particle.setMass(2);                    // 2kg
-            particle.velocity.set(0, 0, 35);        // 35m/s
+            particle.velocity.set(35, 0, 0);        // 35m/s
             particle.acceleration.set(0, -1, 0);    // little gravity
             particle.setDamping(0.99);              // damping factor
             break;
 
         case 'artillery':
+            particle.color = 'blue';
             particle.setMass(200);                  // 200kg
-            particle.velocity.set(0, 30, 40);       // 50m/s
+            particle.velocity.set(30, 40, 0);       // 50m/s
             particle.acceleration.set(0, -20, 0);   // high gravity
             particle.setDamping(0.99);              // damping factor
             break;
 
         case 'fireball':
+            particle.color = 'orange';
             particle.setMass(1);                    // 1kg
-            particle.velocity.set(0, 0, 10);        // 5m/s
+            particle.velocity.set(10, 0, 0);        // 5m/s
             particle.acceleration.set(0, 0.6, 0);   // negative gravity!
             particle.setDamping(0.99);              // damping factor
             break;
 
         case 'laser':
+            particle.color = 'red';
             particle.setMass(0.1);                  // 2kg
-            particle.velocity.set(0, 0, 100);       // 35m/s
+            particle.velocity.set(100, 0, 0);       // 35m/s
             particle.acceleration.set(0, 0, 0);     // no gravity
             particle.setDamping(0.99);              // damping factor
             break;
     }
+    particle.position.set(100, 100, 0);
     return particle;
 };
 Ape.ParticleFactory.PISTOL = 'pistol';
