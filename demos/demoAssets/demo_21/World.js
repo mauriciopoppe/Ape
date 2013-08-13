aaa2dd0eaabbbb17508f72d5435179e40905df7b
Mauricio Poppe <me@maurizzzio.com>
2013-08-13 19:41:06 -0400

    Added a cylinder in the spring example that uses rigid bodies

--- demos/demoAssets/demo_21/World.js
+++ demos/demoAssets/demo_21/World.js
@@ -13,7 +13,7 @@ Ape.World = T3.World.extend({
 
     initWorld: function () {
         this.bodies = [];
-        this.restLength = 20;
+        this.restLength = 15;
 
         // static body
         var staticBody = Ape.RigidBodyFactory(
@@ -28,7 +28,7 @@ Ape.World = T3.World.extend({
             Ape.RigidBodyFactory.SIMPLE
         );
         gravityBody.setMass(10);
-        gravityBody.angularDamping = 0.4;
+        gravityBody.angularDamping = 0.5;
         gravityBody.position.set(210, 100, 10);
         scene.add(gravityBody);
 
@@ -47,7 +47,7 @@ Ape.World = T3.World.extend({
             new Ape.Spring(
                 new THREE.Vector3(-2.5, 0, 0),      // localConnectionPoint
                 staticBody,                         // other
-                new THREE.Vector3(0,-2.5, 0),       // otherConnectionPoint
+                new THREE.Vector3(0, -2.5, 0),      // otherConnectionPoint
                 20,                                 // spring constant
                 this.restLength                     // rest length
             )
@@ -57,7 +57,9 @@ Ape.World = T3.World.extend({
     },
 
     update: function (delta) {
-        var me = this;
+        var me = this,
+            staticBody = this.bodies[0],
+            gravityBody = this.bodies[1];
 
         me._super(delta);
 
@@ -65,6 +67,60 @@ Ape.World = T3.World.extend({
         this.bodies.forEach(function (body) {
             body.integrate(delta);
         });
-    }
 
+        // draw a cylinder between the contact points
+        this.drawCylinder(
+            gravityBody.getPointInWorldSpace(
+                new THREE.Vector3(-2.5, 0, 0)
+            ),
+            staticBody.getPointInWorldSpace(
+                new THREE.Vector3(0, -2.5, 0)
+            ),
+            0
+        );
+    },
+
+    drawCylinder: function (vstart, vend, index) {
+        var me = this,
+            orientation,
+            position,
+            offsetRotation,
+            distance,
+            geometry,
+            mesh;
+
+        me.mesh = me.mesh || [];
+        mesh = me.mesh[index];
+        position = vend.clone().add(vstart).divideScalar(2);
+        distance = vstart.distanceTo(vend);
+
+        if (!mesh) {
+            geometry = new THREE.CylinderGeometry(0.5, 0.5, distance, 8, 1, false);
+            me.mesh[index] = mesh = new THREE.Mesh(
+                geometry,
+                new THREE.MeshBasicMaterial({
+                    wireframe: true,
+                    color: 0xaaaaff
+                })
+            );
+            mesh.wireframe = true;
+            me.mesh[index].originalDistance = distance;
+            scene.add(mesh);
+        }
+        // inverse the internal matrix to revert the previous changes
+        var matrix = mesh.matrix.clone();
+        mesh.applyMatrix(new THREE.Matrix4().getInverse(matrix));
+
+        // reapply a new rotation
+        orientation = new THREE.Matrix4();
+        offsetRotation = new THREE.Matrix4();
+        orientation.lookAt(vstart, vend, new THREE.Vector3(0, 1, 0));
+        offsetRotation.makeRotationX(Math.PI * 0.5);
+        orientation.multiply(offsetRotation);
+        mesh.applyMatrix(orientation);
+
+        mesh.scale.set(1, distance / mesh.originalDistance, 1);
+        mesh.position = position;
+
+    }
 });
